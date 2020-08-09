51a457eac688615e0e3416ce9fb36346e1e5f7a5
jpalmat <jimmy.palma1990@gmail.com>
2020-08-09 01:03:51 -0500

    SortArrayByParityII

---
+++ src/leetcode/array/SortArrayByParityII/SortArrayByParityII.java
@@ -0,0 +1,26 @@
+package leetcode.array.SortArrayByParityII;
+
+public class SortArrayByParityII {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] sortArrayByParityII(int[] A) {
+        int N = A.length;
+        int[] ans = new int[N];
+
+        int t = 0;
+        for (int x: A) if (x % 2 == 0) {
+            ans[t] = x;
+            t += 2;
+        }
+
+        t = 1;
+        for (int x: A) if (x % 2 == 1) {
+            ans[t] = x;
+            t += 2;
+        }
+
+        return ans;
+    }
+}
